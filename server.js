f97997b79d659d9a5c048b341adbdb5961f74462
Oliver Weiler <weiler@predic8.de>
2016-10-12 16:45:52 +0200

    Komplettes Beispiel

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
-const Eureka = require('eureka-js-client').Eureka;
+'use strict';
 
-// example configuration
-const client = new Eureka({
-  // application instance information
-  instance: {
-    app: 'likes-service',
-    hostName: 'localhost',
-    ipAddr: '127.0.0.1',
-    statusPageUrl: 'http://localhost:8080/info',
-    port: {
-      '$': 8080,
-      '@enabled': true
-    },
-    vipAddress: 'jq.test.something.com',
-    dataCenterInfo: {
-      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-      name: 'MyOwn',
-    }
-  },
-  eureka: {
-    // eureka server host / port
-    host: 'localhost',
-    port: 8761,
-    servicePath: '/eureka/apps/'
+const Hapi = require('hapi');
+const redis = require("redis"), client = redis.createClient({host: "172.17.0.2"});
+
+const useEur = require('./useEur');
+
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+
+
+Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({ 
+  host: 'localhost', 
+  port: 8000 
+});
+
+// Add the route
+server.route({
+  method: 'POST',
+  path:'/likes/objects/{uri}', 
+  handler: function (request, reply) {
+    client.incr(request.params.uri);
+    //    client.set("test", "string val", redis.print);
+    return reply(request.params.uri);
   }
 });
 
-client.start();
+server.route({
+  method: 'GET',
+  path:'/likes/objects/{uri}', 
+  handler: function (request, reply) {
+    client.get(request.params.uri, (err,likes) => {
+      if (err) throw (err);
+      return reply({likes});        // Kurzschreibweise für {likes:likes}
+    });
 
-console.log('Likes service');
+  }
+});
+
+
+// Start the server
+server.start((err) => {
+
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
+
